e2c0a46b58dc6ec1643e53fee8bec0ac5a89b5dc
jianbohu <hu.j@husky.neu.edu>
2016-09-24 16:11:35 -0400

    Remove Sample Code

--- client/src/service/service.js
+++ client/src/service/service.js
@@ -1,26 +1,4 @@
 export default function service($resource){
- /*
- * Sample Code
- */
-  angular.module('hyperion')
-  .factory('Brands', ($resource)=>{
-    return $resource('/brand',null,{
-      'update' : { method:'PUT' },
-      'save'  : {method : 'POST'}
-    });
-  });
-
-  angular.module('hyperion')
-  .factory('Storage', ($resource)=>{
-    return $resource('/storage',null,{
-      'update' : { method:'PUT' },
-      'get' : {method : 'Get' , isArray : false},
-      'save'  : {method : 'POST'}
-    });
-  });
-  /*
-  * End of Sample Code
-  */
 
   let module = angular.module('hyperion')
   module.factory('AddNewService',($resource) => {
