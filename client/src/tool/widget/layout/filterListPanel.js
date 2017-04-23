c7d6967883e41c00f18a94056700ca5df49b43b0
jianbohu <hu.j@husky.neu.edu>
2017-04-23 12:09:10 -0400

    Fix component not display problem for filter list component by moving logic to $onInit

--- client/src/tool/widget/layout/filterListPanel.js
+++ client/src/tool/widget/layout/filterListPanel.js
@@ -14,25 +14,34 @@ export default function(moduleName){
 function filterListPanelCtl(ObjectService,$scope,$state){
   let ctrl = this;
   ctrl.store = [];
-  ObjectService.query(
-    {'objType' : ctrl.fieldDef.objType},
-    {'status' : 'open'},
-    (list)=>{
-    list.forEach((item)=>{
-      ctrl.store.push(item);
-      console.log('p');
+
+  ctrl.$onInit = ()=>{
+
+    ObjectService.query(
+      {'objType' : ctrl.fieldDef.objType},
+      {'status' : 'open'},
+      (list)=>{
+      list.forEach((item)=>{
+        ctrl.store.push(item);
+        console.log('p');
+      });
     });
-  });
-  $scope.header = ctrl.fieldDef.label;
-  $scope.printLabel = printLabel;
-  $scope.displayConfig = ctrl.fieldDef.displayConfig;
-  $scope.store = ctrl.store;
-  $scope.onItemClick = (item)=>{
-    if(ctrl.fieldDef.target && ctrl.fieldDef.targetCfg){
-      let param = {};
-      param[ctrl.fieldDef.targetCfg.targetKey] = item[ctrl.fieldDef.targetCfg.itemKey];
-      $state.go(ctrl.fieldDef.target,param);
-    }
-  };
+
+    $scope.header = ctrl.fieldDef.label;
+    $scope.printLabel = printLabel;
+    $scope.displayConfig = ctrl.fieldDef.displayConfig;
+    $scope.store = ctrl.store;
+    $scope.onItemClick = (item)=>{
+      if(ctrl.fieldDef.target && ctrl.fieldDef.targetCfg){
+        let param = {};
+        param[ctrl.fieldDef.targetCfg.targetKey] = item[ctrl.fieldDef.targetCfg.itemKey];
+        $state.go(ctrl.fieldDef.target,param);
+      }
+    };
+  }
+
+
+
+
   console.log(ctrl);
 }
