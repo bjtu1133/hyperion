dac481e41a0bf9051c2cec96c09a3a3a893e82c5
jianbohu <hu.j@husky.neu.edu>
2017-04-29 21:46:21 -0400

    Add init to optionsPanel

--- client/src/tool/widget/assocPicker/optionsPanel.js
+++ client/src/tool/widget/assocPicker/optionsPanel.js
@@ -18,19 +18,20 @@ export default function(moduleName){
 }
 function optionsPanelCtl($scope){
   let ctrl = this;
-  let titleCfg = ctrl.fieldDef.optionConfig.title;
-  $scope.list = ctrl.options;
-  $scope.optionCfg = ctrl.fieldDef.optionConfig
-  $scope.idField=ctrl.fieldDef.idField;
-  $scope.selectedItemIds = ctrl.selectedItemIds;
-  $scope.numOfItems = 10;
-  $scope.printLabel = printLabel;
+  ctrl.$onInit = ()=>{
+    let titleCfg = ctrl.fieldDef.optionConfig.title;
+    $scope.list = ctrl.options;
+    $scope.optionCfg = ctrl.fieldDef.optionConfig
+    $scope.idField=ctrl.fieldDef.idField;
+    $scope.selectedItemIds = ctrl.selectedItemIds;
+    $scope.numOfItems = 10;
+    $scope.printLabel = printLabel;
 
-  $scope.select = (item)=>{
-    $scope.selectedItemIds.push(item[$scope.idField]);
-    ctrl.selectedItems.push(item);
-    ctrl.onSelected();
-  };
-
+    $scope.select = (item)=>{
+      $scope.selectedItemIds.push(item[$scope.idField]);
+      ctrl.selectedItems.push(item);
+      ctrl.onSelected();
+    };
+  }
   //console.log(ctrl);
 }
